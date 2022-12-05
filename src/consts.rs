6b828e742d7ad753a8d22e5de549642e8e68eda3
Calin Cocan <calinrc@gmail.com>
2022-12-05 16:11:46 +0200

    update plugin exports

--- src/consts.rs
+++ src/consts.rs
@@ -4,128 +4,131 @@ use std::ffi::c_uint;
 use std::ffi::c_void;
 
 pub const FS_FILE_OK: c_int = 0;
-const FS_FILE_EXISTS: c_int = 1;
-const FS_FILE_NOTFOUND: c_int = 2;
-const FS_FILE_READERROR: c_int = 3;
-const FS_FILE_WRITEERROR: c_int = 4;
-const FS_FILE_USERABORT: c_int = 5;
-const FS_FILE_NOTSUPPORTED: c_int = 6;
-const FS_FILE_EXISTSRESUMEALLOWED: c_int = 7;
-const FS_EXEC_OK: c_int = 0;
-const FS_EXEC_ERROR: c_int = 1;
-const FS_EXEC_YOURSELF: c_int = -1;
-const FS_EXEC_SYMLINK: c_int = -2;
-const FS_COPYFLAGS_OVERWRITE: c_int = 1;
-const FS_COPYFLAGS_RESUME: c_int = 2;
-const FS_COPYFLAGS_MOVE: c_int = 4;
-const FS_COPYFLAGS_EXISTS_SAMECASE: c_int = 8;
-const FS_COPYFLAGS_EXISTS_DIFFERENTCASE: c_int = 16;
+pub const FS_FILE_EXISTS: c_int = 1;
+pub const FS_FILE_NOTFOUND: c_int = 2;
+pub const FS_FILE_READERROR: c_int = 3;
+pub const FS_FILE_WRITEERROR: c_int = 4;
+pub const FS_FILE_USERABORT: c_int = 5;
+pub const FS_FILE_NOTSUPPORTED: c_int = 6;
+pub const FS_FILE_EXISTSRESUMEALLOWED: c_int = 7;
+pub const FS_EXEC_OK: c_int = 0;
+pub const FS_EXEC_ERROR: c_int = 1;
+pub const FS_EXEC_YOURSELF: c_int = -1;
+pub const FS_EXEC_SYMLINK: c_int = -2;
+pub const FS_COPYFLAGS_OVERWRITE: c_int = 1;
+pub const FS_COPYFLAGS_RESUME: c_int = 2;
+pub const FS_COPYFLAGS_MOVE: c_int = 4;
+pub const FS_COPYFLAGS_EXISTS_SAMECASE: c_int = 8;
+pub const FS_COPYFLAGS_EXISTS_DIFFERENTCASE: c_int = 16;
 
 // flags for tRequestProc
-const RT_Other: c_int = 0;
-const RT_UserName: c_int = 1;
-const RT_Password: c_int = 2;
-const RT_Account: c_int = 3;
-const RT_UserNameFirewall: c_int = 4;
-const RT_PasswordFirewall: c_int = 5;
-const RT_TargetDir: c_int = 6;
-const RT_URL: c_int = 7;
-const RT_MsgOK: c_int = 8;
-const RT_MsgYesNo: c_int = 9;
-const RT_MsgOKCancel: c_int = 10;
+pub const RT_Other: c_int = 0;
+pub const RT_UserName: c_int = 1;
+pub const RT_Password: c_int = 2;
+pub const RT_Account: c_int = 3;
+pub const RT_UserNameFirewall: c_int = 4;
+pub const RT_PasswordFirewall: c_int = 5;
+pub const RT_TargetDir: c_int = 6;
+pub const RT_URL: c_int = 7;
+pub const RT_MsgOK: c_int = 8;
+pub const RT_MsgYesNo: c_int = 9;
+pub const RT_MsgOKCancel: c_int = 10;
 
 // flags for tLogProc
-const MSGTYPE_CONNECT: c_int = 1;
-const MSGTYPE_DISCONNECT: c_int = 2;
-const MSGTYPE_DETAILS: c_int = 3;
-const MSGTYPE_TRANSFERCOMPLETE: c_int = 4;
-const MSGTYPE_CONNECTCOMPLETE: c_int = 5;
-const MSGTYPE_IMPORTANTERROR: c_int = 6;
-const MSGTYPE_OPERATIONCOMPLETE: c_int = 7;
+pub const MSGTYPE_CONNECT: c_int = 1;
+pub const MSGTYPE_DISCONNECT: c_int = 2;
+pub const MSGTYPE_DETAILS: c_int = 3;
+pub const MSGTYPE_TRANSFERCOMPLETE: c_int = 4;
+pub const MSGTYPE_CONNECTCOMPLETE: c_int = 5;
+pub const MSGTYPE_IMPORTANTERROR: c_int = 6;
+pub const MSGTYPE_OPERATIONCOMPLETE: c_int = 7;
 
 // flags for FsStatusInfo
-const FS_STATUS_START: c_int = 0;
-const FS_STATUS_END: c_int = 1;
-const FS_STATUS_OP_LIST: c_int = 1;
-const FS_STATUS_OP_GET_SINGLE: c_int = 2;
-const FS_STATUS_OP_GET_MULTI: c_int = 3;
-const FS_STATUS_OP_PUT_SINGLE: c_int = 4;
-const FS_STATUS_OP_PUT_MULTI: c_int = 5;
-const FS_STATUS_OP_RENMOV_SINGLE: c_int = 6;
-const FS_STATUS_OP_RENMOV_MULTI: c_int = 7;
-const FS_STATUS_OP_DELETE: c_int = 8;
-const FS_STATUS_OP_ATTRIB: c_int = 9;
-const FS_STATUS_OP_MKDIR: c_int = 10;
-const FS_STATUS_OP_EXEC: c_int = 11;
-const FS_STATUS_OP_CALCSIZE: c_int = 12;
-const FS_STATUS_OP_SEARCH: c_int = 13;
-const FS_STATUS_OP_SEARCH_TEXT: c_int = 14;
-const FS_STATUS_OP_SYNC_SEARCH: c_int = 15;
-const FS_STATUS_OP_SYNC_GET: c_int = 16;
-const FS_STATUS_OP_SYNC_PUT: c_int = 17;
-const FS_STATUS_OP_SYNC_DELETE: c_int = 18;
-const FS_ICONFLAG_SMALL: c_int = 1;
-const FS_ICONFLAG_BACKGROUND: c_int = 2;
-const FS_ICON_USEDEFAULT: c_int = 0;
-const FS_ICON_EXTRACTED: c_int = 1;
-const FS_ICON_EXTRACTED_DESTROY: c_int = 2;
-const FS_ICON_DELAYED: c_int = 3;
-const FS_BITMAP_NONE: c_int = 0;
-const FS_BITMAP_EXTRACTED: c_int = 1;
-const FS_BITMAP_EXTRACT_YOURSELF: c_int = 2;
-const FS_BITMAP_EXTRACT_YOURSELF_ANDDELETE: c_int = 3;
-const FS_BITMAP_CACHE: c_int = 256;
-const FS_CRYPT_SAVE_PASSWORD: c_int = 1;
-const FS_CRYPT_LOAD_PASSWORD: c_int = 2;
-const FS_CRYPT_LOAD_PASSWORD_NO_UI: c_int = 3; // Load password only if master password has already been entered!
-const FS_CRYPT_COPY_PASSWORD: c_int = 4; // Copy encrypted password to new connection name
-const FS_CRYPT_MOVE_PASSWORD: c_int = 5; // Move password when renaming a connection
-const FS_CRYPT_DELETE_PASSWORD: c_int = 6; // Delete password
-const FS_CRYPTOPT_MASTERPASS_SET: c_int = 1; // The user already has a master password defined
+pub const FS_STATUS_START: c_int = 0;
+pub const FS_STATUS_END: c_int = 1;
+pub const FS_STATUS_OP_LIST: c_int = 1;
+pub const FS_STATUS_OP_GET_SINGLE: c_int = 2;
+pub const FS_STATUS_OP_GET_MULTI: c_int = 3;
+pub const FS_STATUS_OP_PUT_SINGLE: c_int = 4;
+pub const FS_STATUS_OP_PUT_MULTI: c_int = 5;
+pub const FS_STATUS_OP_RENMOV_SINGLE: c_int = 6;
+pub const FS_STATUS_OP_RENMOV_MULTI: c_int = 7;
+pub const FS_STATUS_OP_DELETE: c_int = 8;
+pub const FS_STATUS_OP_ATTRIB: c_int = 9;
+pub const FS_STATUS_OP_MKDIR: c_int = 10;
+pub const FS_STATUS_OP_EXEC: c_int = 11;
+pub const FS_STATUS_OP_CALCSIZE: c_int = 12;
+pub const FS_STATUS_OP_SEARCH: c_int = 13;
+pub const FS_STATUS_OP_SEARCH_TEXT: c_int = 14;
+pub const FS_STATUS_OP_SYNC_SEARCH: c_int = 15;
+pub const FS_STATUS_OP_SYNC_GET: c_int = 16;
+pub const FS_STATUS_OP_SYNC_PUT: c_int = 17;
+pub const FS_STATUS_OP_SYNC_DELETE: c_int = 18;
+pub const FS_ICONFLAG_SMALL: c_int = 1;
+pub const FS_ICONFLAG_BACKGROUND: c_int = 2;
+pub const FS_ICON_USEDEFAULT: c_int = 0;
+pub const FS_ICON_EXTRACTED: c_int = 1;
+pub const FS_ICON_EXTRACTED_DESTROY: c_int = 2;
+pub const FS_ICON_DELAYED: c_int = 3;
+pub const FS_BITMAP_NONE: c_int = 0;
+pub const FS_BITMAP_EXTRACTED: c_int = 1;
+pub const FS_BITMAP_EXTRACT_YOURSELF: c_int = 2;
+pub const FS_BITMAP_EXTRACT_YOURSELF_ANDDELETE: c_int = 3;
+pub const FS_BITMAP_CACHE: c_int = 256;
+pub const FS_CRYPT_SAVE_PASSWORD: c_int = 1;
+pub const FS_CRYPT_LOAD_PASSWORD: c_int = 2;
+pub const FS_CRYPT_LOAD_PASSWORD_NO_UI: c_int = 3; // Load password only if master password has already been entered!
+pub const FS_CRYPT_COPY_PASSWORD: c_int = 4; // Copy encrypted password to new connection name
+pub const FS_CRYPT_MOVE_PASSWORD: c_int = 5; // Move password when renaming a connection
+pub const FS_CRYPT_DELETE_PASSWORD: c_int = 6; // Delete password
+pub const FS_CRYPTOPT_MASTERPASS_SET: c_int = 1; // The user already has a master password defined
 
 // flags for FsFindFirst/FsFindNext
-const FILE_ATTRIBUTE_DIRECTORY: c_uint = 0x00000010;
-const FILE_ATTRIBUTE_REPARSE_POINT: c_uint = 0x00000400;
-const FILE_ATTRIBUTE_UNIX_MODE: c_uint = 0x80000000;
+pub const FILE_ATTRIBUTE_DIRECTORY: c_uint = 0x00000010;
+pub const FILE_ATTRIBUTE_REPARSE_POINT: c_uint = 0x00000400;
+pub const FILE_ATTRIBUTE_UNIX_MODE: c_uint = 0x80000000;
 
 pub type TProgressProc = unsafe extern "C" fn(c_int, *mut c_char, *mut c_char, c_int) -> c_int;
 pub type TLogProc = unsafe extern "C" fn(c_int, c_int, *mut c_char) -> c_void;
 pub type TRequestProc =
     unsafe extern "C" fn(c_int, c_int, *mut c_char, *mut c_char, *mut c_char, c_int) -> c_int;
 
-type HANDLE = *mut c_void;
+pub type HANDLE = *mut c_void;
+pub type HWND = HANDLE;
 pub const INVALID_HANDLE: HANDLE = -1isize as HANDLE; // {0xffffffffffffffff as *mut core::ffi::c_void}
 
 pub type DWORD = c_uint;
-struct FILETIME {
+pub struct FILETIME {
     dw_low_date_time: DWORD,
     dw_high_date_time: DWORD,
 }
 
-// typedef struct _WIN32_FIND_DATAA {
-// 	DWORD dwFileAttributes;
-// 	FILETIME ftCreationTime;
-// 	FILETIME ftLastAccessTime;
-// 	FILETIME ftLastWriteTime;
-// 	DWORD nFileSizeHigh;
-// 	DWORD nFileSizeLow;
-// 	DWORD dwReserved0;
-// 	DWORD dwReserved1;
-// 	CHAR cFileName[MAX_PATH];
-// 	CHAR cAlternateFileName[14];
-// } WIN32_FIND_DATAA,*LPWIN32_FIND_DATAA;
-
 pub const MAX_PATH: usize = 260;
+pub type BOOL = c_uint;
 
-struct WIN32_FIND_DATAA {
-    dwFileAttributes: DWORD,
-    ftCreationTime: FILETIME,
-    ftLastAccessTime: FILETIME,
-    ftLastWriteTime: FILETIME,
-    nFileSizeHigh: DWORD,
-    nFileSizeLow: DWORD,
-    dwReserved0: DWORD,
-    dwReserved1: DWORD,
-    cFileName: [c_char; MAX_PATH],
-    cAlternateFileName: [c_char; 14],
+pub struct WIN32_FIND_DATAA {
+    dw_file_attributes: DWORD,
+    ft_creation_time: FILETIME,
+    ft_last_access_time: FILETIME,
+    ft_last_write_wime: FILETIME,
+    n_file_size_high: DWORD,
+    n_file_size_low: DWORD,
+    dw_reserved_0: DWORD,
+    dw_reserved_1: DWORD,
+    c_file_name: [c_char; MAX_PATH],
+    c_alternate_file_name: [c_char; 14],
+}
+
+pub struct RemoteInfoStruct {
+    size_low: DWORD,
+    size_high: DWORD,
+    last_write_time: FILETIME,
+    attr: c_int,
+}
+
+pub struct FsDefaultParamStruct {
+    size: c_int,
+    plugin_interface_version_low: DWORD,
+    plugin_interface_version_hi: DWORD,
+    default_ini_name: [c_char; MAX_PATH],
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,20 @@
-use libc;
+use consts::FsDefaultParamStruct;
+use consts::RemoteInfoStruct;
+use consts::TLogProc;
+use consts::TProgressProc;
+use consts::TRequestProc;
+use consts::FILETIME;
+use consts::FS_EXEC_OK;
+use consts::FS_FILE_OK;
+use consts::HANDLE;
+use consts::HWND;
+use consts::INVALID_HANDLE;
+use consts::WIN32_FIND_DATAA;
+
+use consts::BOOL;
 use std::cell::RefCell;
+use std::ffi::c_char;
 use std::ffi::c_int;
-use std::ffi::c_void;
-use std::os::raw::c_char;
-use std::sync::Mutex;
-use std::sync::Once;
 
 mod consts;
 
@@ -19,16 +29,16 @@ mod consts;
 // typedef int BOOL;
 
 thread_local!(static G_PLUGIN_NO: RefCell<Option<c_int>>  = RefCell::new(None));
-thread_local!(static G_PROGRESS_PROC: RefCell<Option<consts::TProgressProc> >  = RefCell::new(None));
-thread_local!(static G_LOG_PROC: RefCell<Option<consts::TLogProc> >  = RefCell::new(None));
-thread_local!(static G_REQUEST_PROC: RefCell<Option<consts::TRequestProc> >  = RefCell::new(None));
+thread_local!(static G_PROGRESS_PROC: RefCell<Option<TProgressProc> >  = RefCell::new(None));
+thread_local!(static G_LOG_PROC: RefCell<Option<TLogProc> >  = RefCell::new(None));
+thread_local!(static G_REQUEST_PROC: RefCell<Option<TRequestProc> >  = RefCell::new(None));
 
 #[no_mangle]
 pub unsafe extern "C" fn FsInit(
     plugin_nr: c_int,
-    p_progress_proc: consts::TProgressProc,
-    p_log_proc: consts::TLogProc,
-    p_request_proc: consts::TRequestProc,
+    p_progress_proc: TProgressProc,
+    p_log_proc: TLogProc,
+    p_request_proc: TRequestProc,
 ) -> c_int {
     G_PLUGIN_NO.with(|plug_no| {
         *plug_no.borrow_mut() = Some(plugin_nr);
@@ -46,10 +56,111 @@ pub unsafe extern "C" fn FsInit(
     0
 }
 
-// #[no_mangle]
-// pub unsafe extern "C" fn FsFindFirst(char* Path, WIN32_FIND_DATAA *FindData) -> c_void{
+#[no_mangle]
+pub unsafe extern "C" fn FsFindFirst(
+    path: *mut c_char,
+    find_data: Option<Box<WIN32_FIND_DATAA>>,
+) -> HANDLE {
+    INVALID_HANDLE
+}
 
-// }
+#[no_mangle]
+pub unsafe extern "C" fn FsFindNext(
+    hdl: HANDLE,
+    find_data: Option<Box<WIN32_FIND_DATAA>>,
+) -> c_int {
+    0
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn FsFindClose(hdl: HANDLE) -> c_int {
+    FS_FILE_OK
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn FsMkDir(path: *mut c_char) -> BOOL {
+    0
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn FsRemoveDir(remote_name: *mut c_char) -> BOOL {
+    0
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn FsRenMovFile(
+    old_name: *mut c_char,
+    new_name: *mut c_char,
+    mmove: BOOL,
+    over_write: BOOL,
+    ri: Option<Box<RemoteInfoStruct>>,
+) -> c_int {
+    FS_FILE_OK
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn FsGetFile(
+    remote_name: *mut c_char,
+    local_name: *mut c_char,
+    copy_flags: c_int,
+    ri: Option<Box<RemoteInfoStruct>>,
+) -> c_int {
+    FS_FILE_OK
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn FsPutFile(
+    local_name: *mut c_char,
+    remote_name: *mut c_char,
+    copy_flags: c_int,
+) -> c_int {
+    FS_FILE_OK
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn FsExecuteFile(
+    main_win: HWND,
+    remote_name: *mut c_char,
+    verb: *mut c_char,
+) -> c_int {
+    FS_EXEC_OK
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn FsDeleteFile(remote_name: *mut c_char) -> BOOL {
+    0
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn FsSetTime(
+    remote_name: *mut c_char,
+    creation_time: *mut FILETIME,
+    last_access_time: *mut FILETIME,
+    last_write_time: *mut FILETIME,
+) -> BOOL {
+    0
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn FsDisconnect(disconnect_root: *mut c_char) -> BOOL {
+    0
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn FsSetDefaultParams(dps: Option<Box<FsDefaultParamStruct>>) {}
+
+#[no_mangle]
+pub unsafe extern "C" fn FsGetDefRootName(def_root_name: *mut c_char, maxlen: c_int) {
+    let plugin_name = "k8s";
+    let bytes = plugin_name.as_bytes();
+    let len = bytes.len();
+    std::ptr::copy(
+        plugin_name.as_bytes().as_ptr().cast(),
+        def_root_name,
+        maxlen as usize,
+    );
+    std::ptr::write(def_root_name.offset(len as isize) as *mut u8, 0u8);
+}
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
