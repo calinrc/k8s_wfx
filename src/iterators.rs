e3b1193e5c5d1506c6178498c0964e2b6b83c712
Calin Cocan <calinrc@gmail.com>
2023-03-01 16:19:21 +0200

    refactor

--- src/iterators.rs
+++ src/iterators.rs
@@ -1,58 +1,56 @@
-use crate::resources;
-use std::slice::Iter;
-use crate::consts::WIN32_FIND_DATAA;
-use crate::consts::FILETIME;
 use crate::consts;
+use crate::consts::FILETIME;
+use crate::consts::WIN32_FIND_DATAA;
+use crate::iterators::pods::PodsIterator;
+use crate::resources;
 use std::path::Path;
+use std::slice::Iter;
 
 pub mod pods;
 
-#[derive(Debug)]
-#[derive(Default)]
+#[derive(Debug, Default)]
 pub struct ReasourceData;
 
-pub trait FindDataUpdater{
+pub trait FindDataUpdater {
     unsafe fn update_find_data(&self, find_data: *mut WIN32_FIND_DATAA);
 }
 
-
-trait FindDataUpdaterIterator: Iterator + FindDataUpdater {}
+pub trait FindDataUpdaterIterator: Iterator + FindDataUpdater {}
 
 pub struct ResourcesItertatorFactory;
 
 impl ResourcesItertatorFactory {
-    pub fn new(path: &Path) -> ResourcesIterator {
-        ResourcesIterator{
-            it : Box::new(resources::K8SResources::iterator()),
-            next_elem: None
-
+    pub fn new(_path: &Path) -> impl Iterator {
+        if _path.parent().is_none() {
+            ResourcesIterator {
+                it: Box::new(resources::K8SResources::iterator()),
+                next_elem: None,
+            }
+        } else {
+            PodsIterator {}
         }
     }
 }
 
 #[derive(Debug)]
-pub struct ResourcesIterator<'a>{
-    
+pub struct ResourcesIterator<'a> {
     it: Box<Iter<'static, resources::K8SResources>>,
-    next_elem : Option<&'a resources::K8SResources>,
-
+    next_elem: Option<&'a resources::K8SResources>,
 }
 
-impl Iterator for ResourcesIterator<'_>{
+impl Iterator for ResourcesIterator<'_> {
     type Item = ReasourceData;
     fn next(&mut self) -> Option<ReasourceData> {
         let result = self.it.next();
         self.next_elem = result;
 
-        if self.next_elem.is_none(){
+        if self.next_elem.is_none() {
             None
-        }
-        else{
+        } else {
             Some(ReasourceData::default())
         }
-    }   
+    }
 }
-
 
 impl Drop for ResourcesIterator<'_> {
     fn drop(&mut self) {
@@ -60,41 +58,41 @@ impl Drop for ResourcesIterator<'_> {
     }
 }
 
-
-impl FindDataUpdater for ResourcesIterator<'_>{
-
+impl FindDataUpdater for ResourcesIterator<'_> {
     unsafe fn update_find_data(&self, find_data: *mut WIN32_FIND_DATAA) {
         match self.next_elem {
             Some(res) => {
-                (*find_data).dw_file_attributes = consts::FILE_ATTRIBUTE_UNIX_MODE | consts::FILE_ATTRIBUTE_DIRECTORY;
+                (*find_data).dw_file_attributes =
+                    consts::FILE_ATTRIBUTE_UNIX_MODE | consts::FILE_ATTRIBUTE_DIRECTORY;
                 (*find_data).ft_creation_time = FILETIME::default();
-                (*find_data).ft_last_access_time =  FILETIME::default();
-                (*find_data).ft_last_write_time =  FILETIME::default();
-                (*find_data).n_file_size_high =  0;
-                (*find_data).n_file_size_low =  0;
-                (*find_data).dw_reserved_0= 0;
-                (*find_data).dw_reserved_1= 0;
+                (*find_data).ft_last_access_time = FILETIME::default();
+                (*find_data).ft_last_write_time = FILETIME::default();
+                (*find_data).n_file_size_high = 0;
+                (*find_data).n_file_size_low = 0;
+                (*find_data).dw_reserved_0 = 0;
+                (*find_data).dw_reserved_1 = 0;
                 let res_str = res.to_string();
-                let bytes =res_str.as_bytes();
+                let bytes = res_str.as_bytes();
                 let len = bytes.len();
-        
+
                 std::ptr::copy(
                     bytes.as_ptr().cast(),
                     (*find_data).c_file_name.as_mut_ptr(),
                     consts::MAX_PATH,
                 );
-                std::ptr::write((*find_data).c_file_name.as_mut_ptr().offset(len as isize) as *mut u8, 0u8);
-        
+                std::ptr::write(
+                    (*find_data).c_file_name.as_mut_ptr().offset(len as isize) as *mut u8,
+                    0u8,
+                );
+
                 //(*find_data).c_file_name= [0i8;260];
-                (*find_data).c_alternate_file_name =  [0i8;14];
-            
+                (*find_data).c_alternate_file_name = [0i8; 14];
+
                 println!("K8SResources {}", res)
             }
             None => {
                 eprint!("Unable to update_find_data. None resource")
-
             }
         }
-        
     }
 }
--- src/iterators/pods.rs
+++ src/iterators/pods.rs
@@ -1,8 +1,27 @@
+use crate::consts;
+use crate::consts::FILETIME;
+use crate::consts::WIN32_FIND_DATAA;
+use crate::iterators::ReasourceData;
+use crate::resources;
 use k8s_openapi::api::core::v1::Pod;
 use kube::{client::ConfigExt, Api, Client, Config, ResourceExt};
+use std::slice::Iter;
 
+use super::FindDataUpdater;
 
-pub async fn list_pods(namespace:String)->anyhow::Result<Vec<String>> { 
+pub struct PodsIterator{};
+
+impl Iterator for PodsIterator {
+    type Item = ReasourceData;
+    fn next(&mut self) -> Option<ReasourceData> {
+        None
+    }
+}
+impl FindDataUpdater for PodsIterator {
+    unsafe fn update_find_data(&self, find_data: *mut WIN32_FIND_DATAA) {}
+}
+
+pub async fn list_pods(namespace: String) -> anyhow::Result<Vec<String>> {
     let config = Config::infer().await?;
     let https = config.openssl_https_connector()?;
     let mut vec = Vec::new();
@@ -19,5 +38,4 @@ pub async fn list_pods(namespace:String)->anyhow::Result<Vec<String>> {
         //info!("{}", p.name_any());
     }
     Ok(vec)
-
 }
